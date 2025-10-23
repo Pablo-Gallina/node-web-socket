aa602cb1f1bb93a29d82edfeff3415cfac110053
Pablo Gallina <pgil@corporaciondeconfianza.com>
2025-10-23 12:47:10 -0600

    agregado estilos al chat y persitencia de datos

--- server/index.js
+++ server/index.js
@@ -28,11 +28,12 @@ await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT NOT NULL,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user TEXT NOT NULL
   );
 `);
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("a user connected");
 
   socket.on("disconnect", () => {
@@ -41,21 +42,53 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", async (msg) => {
     let result;
+    const username = socket.handshake.auth.username || "Anonymous";
 
     try {
       result = await db.execute({
-        sql: "INSERT INTO messages (content) VALUES (:content)",
-        args: { content: msg },
+        sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+        args: { msg, username },
       });
-
-      console.log(result);
     } catch (error) {
       console.error("Error inserting message:", error);
       return;
     }
 
-    io.emit("chat message", msg, result.lastInsertRowid.toString());
+    const timestampResult = await db.execute({
+      sql: "SELECT CAST(timestamp AS TEXT) as timestamp FROM messages WHERE id = ?",
+      args: [result.lastInsertRowid],
+    });
+    const [{ timestamp }] = timestampResult.rows;
+
+    io.emit(
+      "chat message",
+      msg,
+      result.lastInsertRowid.toString(),
+      username,
+      timestamp
+    );
   });
+
+  if (!socket.recovered) {
+    try {
+      const messages = await db.execute({
+        sql: "SELECT id, content, user, CAST(timestamp AS TEXT) as timestamp FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      });
+
+      messages.rows.forEach(({ id, content, user: username, timestamp }) => {
+        socket.emit(
+          "chat message",
+          content,
+          id.toString(),
+          username,
+          timestamp
+        );
+      });
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  }
 });
 
 app.use(logger("dev"));
